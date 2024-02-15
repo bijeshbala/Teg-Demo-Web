c710a82ae0e2e5ed3c83c7823531c8c593d66b93
Bijesh Balakrishnan <bijeshbala@gmail.com>
2024-02-16 10:12:50 +1100

    Added initila project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Teg_DemoWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
